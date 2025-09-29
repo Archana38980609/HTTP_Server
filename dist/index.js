cdd4e75fc02e2cc06dbf15e06bbc923c90f32b9b
Archana38980609 <archi.dtg@gmail.com>
2025-09-29 19:31:03 +0000

    feat: Add middleware to log non-OK responses

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponse } from "./api/middleware.js";
 const app = express();
 const PORT = 8080;
+app.use(middlewareLogResponse);
 app.use("/app", express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {

c5ff268da99d5240ef4833c8b1560911c340c905
Archana38980609 <archi.dtg@gmail.com>
2025-10-03 18:26:51 +0000

    Creates a new POST endpoint at /api/validate_chirp that checks if the incoming chirp text is 140 characters or less and returns a JSON response indicating validity.

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
 import { middlewareLogResponse, middlewareMetricsInc, } from "./api/middleware.js";
+import { handlerChirpsValidate } from "./api/chirps.js";
 const app = express();
 const PORT = 8080;
 app.use(middlewareLogResponse);
@@ -10,6 +11,7 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
